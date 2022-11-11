0adfd7c542a2e4c38abccfc30848a835c37ca246
Feyza Nisli <feyza.nisli@gmail.com>
2022-11-11 23:02:04 +0300

    Initial commit

---
+++ src/NotOrtalamasi/NotOrtalamasiHesaplayanProgram.java
@@ -0,0 +1,35 @@
+package NotOrtalamasi;
+import java.util.Scanner;
+public class NotOrtalamasiHesaplayanProgram {
+    public static void main(String[] args){
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner not = new Scanner (System.in);
+
+        System.out.print("Matematik Notunuzu Giriniz: ");
+        mat = not.nextInt();
+
+        System.out.print("Fizik Notunuzu Giriniz: ");
+        fizik = not.nextInt();
+
+        System.out.print("Kimya Notunuzu Giriniz: ");
+        kimya = not.nextInt();
+
+        System.out.print("Türkçe Notunuzu Giriniz: ");
+        turkce = not.nextInt();
+
+        System.out.print("Tarih Notunuzu Giriniz: ");
+        tarih = not.nextInt();
+
+        System.out.print("Müzik Notunuzu Giriniz: ");
+        muzik = not.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double sonuc = toplam / 6.0;
+        System.out.println("Ortalamanız: " + sonuc);
+
+        boolean kosul = sonuc >= 60;
+        String karne = kosul ? "Sınıfı Geçtiniz." : "Sınıfta Kaldınız.";
+        System.out.print(karne);
+    }
+}
