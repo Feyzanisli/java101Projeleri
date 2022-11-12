e0959a851ca7dc06cd87b997ffbf6e7430c7efd4
Feyza Nisli <feyza.nisli@gmail.com>
2022-11-12 22:43:33 +0300

    deneme tamam

---
+++ src/Hipotenus/DikUcgendeHipotenusBulanProgram.java
@@ -0,0 +1,16 @@
+package Hipotenus;
+import java.util.Scanner;
+
+public class DikUcgendeHipotenusBulanProgram {
+    public static void main(String[] args){
+        double kenar1, kenar2, hipo;
+        Scanner a = new Scanner(System.in);
+        System.out.print("Dik kenar uzunluklarından 1.sini giriniz: ");
+        kenar1 = a.nextDouble();
+        System.out.print("Dik kenar uzunluklarından 2.sini giriniz: ");
+        kenar2 = a.nextDouble();
+
+        hipo = Math.sqrt((kenar1 * kenar1) + (kenar2 * kenar2));
+        System.out.print("Dik üçgenin hipotenüs uzunluğu: " + hipo);
+    }
+}
