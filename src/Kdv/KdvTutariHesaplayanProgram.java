419866fb885583936388c00cc46935dbb68dc984
Feyza Nisli <feyza.nisli@gmail.com>
2022-11-12 21:12:19 +0300

    son hali

--- src/Kdv/KdvTutariHesaplayanProgram.java
+++ src/Kdv/KdvTutariHesaplayanProgram.java
@@ -1,5 +1,4 @@
 package Kdv;
-import java.awt.*;
 import java.util.Scanner;
 public class KdvTutariHesaplayanProgram {
     public static void main(String[] args){
@@ -9,14 +8,14 @@ public class KdvTutariHesaplayanProgram {
         System.out.print("Ürünün KDV'siz fiyatını giriniz: ");
         fiyat = urun.nextDouble();
 
-        double kdvtutari18 = fiyat / 100 * 18;
-        double kdvarti18 = fiyat + kdvtutari18;
+        double kdvTutari18 = fiyat / 100 * 18;
+        double kdvArti18 = fiyat + kdvTutari18;
 
-        double kdvtutari8 = fiyat / 100 * 8;
-        double kdvarti8 = fiyat + kdvtutari8;
+        double kdvTutari8 = fiyat / 100 * 8;
+        double kdvArti8 = fiyat + kdvTutari8;
 
-        boolean artibin = fiyat > 1000;
-        String hesap = artibin ? ("Ürünün KDV Tutarı: " + kdvtutari8) + ("\nÜrünün KDV'li Fiyatı: " + kdvarti8) : ("Ürünün KDV Tutarı: " + kdvtutari18) + ("\nÜrünün KDV'li Fiyatı: " + kdvarti18);
+        boolean artiBin = fiyat > 1000;
+        String hesap = artiBin ? ("Ürünün KDV Tutarı: " + kdvTutari8) + ("\nÜrünün KDV'li Fiyatı: " + kdvArti8) : ("Ürünün KDV Tutarı: " + kdvTutari18) + ("\nÜrünün KDV'li Fiyatı: " + kdvArti18);
         System.out.println(hesap);
     }
 }
