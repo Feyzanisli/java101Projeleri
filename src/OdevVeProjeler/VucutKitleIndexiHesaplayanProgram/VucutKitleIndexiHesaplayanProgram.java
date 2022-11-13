cb8acb4088b3c798464dbfd407cdb84b2d0cd90e
Feyza Nisli <feyza.nisli@gmail.com>
2022-11-13 23:47:07 +0300

    vki

--- src/OdevVeProjeler/VucutKitleIndexiHesaplayanProgram/VucutKitleIndexiHesaplayanProgram.java
+++ src/OdevVeProjeler/VucutKitleIndexiHesaplayanProgram/VucutKitleIndexiHesaplayanProgram.java
@@ -1,4 +1,4 @@
-package VucutKitleIndexiHesaplayanProgram;
+package OdevVeProjeler.VucutKitleIndexiHesaplayanProgram;
 import java.util.Scanner;
 public class VucutKitleIndexiHesaplayanProgram {
     public static void main(String[] args) {
