eac3456f22d12d62e49417bfd89db18682b60bac
Feyza Nisli <feyza.nisli@gmail.com>
2022-11-13 23:40:12 +0300

    vki

---
+++ src/OdevVeProjeler/VucutKitleIndexiHesaplayanProgram.java
@@ -0,0 +1,19 @@
+package OdevVeProjeler;
+import java.util.Scanner;
+
+//Patika.dev profil adresim: https://app.patika.dev/feyzameyza
+
+public class VucutKitleIndexiHesaplayanProgram {
+    public static void main(String[] args) {
+        double m, kg, index;
+
+        Scanner olcu = new Scanner(System.in);
+        System.out.print("Boyunuzu (metre cinsinde) giriniz: ");
+        m = olcu.nextDouble();
+        System.out.print("Kilonuzu giriniz: ");
+        kg = olcu.nextDouble();
+
+        index = kg / (m * m);
+        System.out.print("Vücut kitle indexiniz: " + index + " 'dir.");
+    }
+}
