4169fa8b1c0887d1bfe3b27fdea02923151dcb59
Feyza Nisli <feyza.nisli@gmail.com>
2022-11-30 21:01:21 +0300

    armstrong

---
+++ src/OdevVeProjeler/ArmstrongSayiBulanProgram/ArmstrongSayiBulanProgram.java
@@ -0,0 +1,27 @@
+package OdevVeProjeler.ArmstrongSayiBulanProgram;
+
+import java.util.Scanner;
+
+public class ArmstrongSayiBulanProgram {
+    public static void main(String[] args) {
+        int number;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen bir sayı giriniz: ");
+        number = input.nextInt();
+
+        int nmbr = number, digitCount = 0, digitNum, total = 0;
+
+        while (nmbr != 0) {
+            nmbr /= 10;
+            digitCount++;
+        }
+
+        for (int i = 1; i <= digitCount; i++) {
+            digitNum = number % 10;
+            total += digitNum;
+            number = number / 10;
+        }
+
+        System.out.println("Sayının basamak sayılarının toplamı: " + total + "'dır.");
+    }
+}
