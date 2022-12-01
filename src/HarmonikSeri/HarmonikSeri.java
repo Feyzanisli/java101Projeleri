051a362eeeaff2af609895c24cefabebc05e4d71
Feyza Nisli <feyza.nisli@gmail.com>
2022-12-01 23:38:44 +0300

    harmonikseri

---
+++ src/HarmonikSeri/HarmonikSeri.java
@@ -0,0 +1,19 @@
+package HarmonikSeri;
+
+import java.util.Scanner;
+
+public class HarmonikSeri {
+    public static void main(String[] args) {
+        int number;
+        double total = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen bir sayı giriniz: ");
+        number = input.nextInt();
+
+        for (double i = 1; i <= number; i++) {
+            total += (1 / i);
+        }
+        System.out.println(number + " 'in harmonik seri toplamı: " + total + " 'dir.");
+    }
+}
